85dac06b707e2e73a702e79000f7499cad20bf8a
GaneshChaturthiCJ <67580297+GaneshChaturthiCJ@users.noreply.github.com>
2023-07-14 12:44:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+require("dotenv").config();
+
 
 // Write a GET route to return the value stored in the NUMBER variable stored in the .env file
 /*
@@ -9,7 +11,14 @@ app.use(express.json());
 }
 */
 app.get('/api/get-env', (req, res) => {
-   //Write your code here
+   const num = process.env.NUMBER
+    if(num){
+        res.status(200).json({
+            "number" : num
+        })
+    } else {
+        res.status(404).json({ "err": "Not Found"})
+    }
 });
 
 module.exports = app;
